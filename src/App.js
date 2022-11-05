4e9f931252a5b3635ac050ce4bcd01314776a523
sho19 <areeb2132@gmail.com>
2022-11-05 15:59:35 +0530

    added component for token input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { useState } from 'react'
 import './App.css';
 import TokenProvider from './token-provider/TokenProvider'
 
 function App() {
+  const [token, setToken] = useState(null);
   return (
     <div className="App">
       <header className="App-header">
         Repo commits
       </header>
-      <TokenProvider />
+      {token? <></> : <TokenProvider setToken={setToken}/>}
+      <TokenProvider setToken={setToken}/>
     </div>
   );
 }
--- src/token-provider/TokenProvider.js
+++ src/token-provider/TokenProvider.js
@@ -1,22 +1,25 @@
 import './TokenProvider.css';
-import {useState} from 'react'
+import { useState } from 'react'
 
-function TokenProvider() {
-    const [token, setToken] = useState("");
+function TokenProvider(props) {
+    const [input, setInput] = useState("");
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert(`The name you entered was: ${token}`)
+        if (input) {
+            sessionStorage['input'] = input;
+            props.setToken(input)
+        }
     }
 
     return (
-        <div className="TokenProvider">
+        <div className="token-provider">
             <form onSubmit={handleSubmit}>
-                <label className='input-label'>Enter your token:
+                <label className='input-label'>Enter your input:
                     <input
                         type="text"
-                        value={token}
-                        onChange={(e) => setToken(e.target.value)}
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
                     />
                 </label>
                 <input type="submit" />
